c8e1b2b9c8bb3a9c9b4e145a9b7127ad92b34b73
MFDGaming <alexandros.argentakis@gmail.com>
2021-12-29 23:24:52 +0200

    Add forgotten file header

--- podrum/network/raknet/socket.c
+++ podrum/network/raknet/socket.c
@@ -1,3 +1,11 @@
+/*
+                   Podrum R3 Copyright MFDGaming & PodrumTeam
+                 This file is licensed under the GPLv2 license.
+              To use this file you must own a copy of the license.
+                       If you do not you can get it from:
+            http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+ */
+
 #ifdef _WIN32
 
 #include <winsock2.h>
